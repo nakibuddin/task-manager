89346ec6c5af2ed362a0fd0c8c47f5aaf6f0fc66
nakibuddin <nakibaust137@gmail.com>
2022-12-30 23:52:56 +0600

    Final

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
     const handleGoogleLogIn = () => {
@@ -51,12 +52,6 @@ const Login = () => {
 					</svg>
 					<p>Continue with Google</p>
 				</button>
-				<button onClick={handleGithubLogIn} aria-label="Login with GitHub" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1  border-gray-400 focus:ring-violet-400">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
-						<path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
-					</svg>
-					<p>Continue with GitHub</p>
-				</button>
 				
 			</div>
 			<div className="flex items-center w-full my-4">
@@ -64,18 +59,19 @@ const Login = () => {
 				<p className="px-3  text-gray-400">OR</p>
 				<hr className="w-full  text-gray-400"/>
 			</div>
+
 			<form onSubmit={handleLogIn} noValidate="" action="" className="space-y-8 ng-untouched ng-pristine ng-valid">
 				<div className="space-y-4">
 					<div className="space-y-2">
 						<label htmlFor="email" className="block text-sm">Email address</label>
-						<input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md  border-gray-700  bg-gray-900  text-gray-100 focus: border-violet-400" required/>
+						<input type="email" name="email" id="email" placeholder="Email" className="w-full px-3 py-2 border rounded-md  border-gray-700  bg-gray-900  text-gray-100 focus: border-violet-400" required/>
 					</div>
 					<div className="space-y-2">
 						<div className="flex justify-between">
 							<label htmlFor="password" className="text-sm">Password</label>
 							{/* <a href='/' rel="noopener noreferrer" className="text-xs hover:underline  text-gray-400">Forgot password?</a> */}
 						</div>
-						<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md  border-gray-700  bg-gray-900  text-gray-100 focus: border-violet-400" required/>
+						<input type="password" name="password" id="password" placeholder="Password" className="w-full px-3 py-2 border rounded-md  border-gray-700  bg-gray-900  text-gray-100 focus: border-violet-400" required/>
 					</div>
 					<div>
 						{/* <p className='text-red-600'>{loginError}</p> */}
@@ -84,6 +80,7 @@ const Login = () => {
 				</div>
 				<button type="submit" className="w-full px-8 py-3 font-semibold rounded-md  bg-violet-400  text-gray-900">Log in</button>
 			</form>
+
 		</div>
     );
 };
